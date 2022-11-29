94b9e5cf02825809ff52a9f9b10177cc1fcc65ae
DeAdrien Hill <deadrienhill@yahoo.com>
2022-11-29 10:09:54 -0600

    Excercise 1 Complete

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -2,9 +2,52 @@
 {
     public class Program
     {
+        //Exercise 1
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = 11;
+
+            var favNumber = r;
+
+            Console.WriteLine("Try to guess my favorite number");
+
+            var userInput = int.Parse(Console.ReadLine());
+
+
+            // If user input is less than print Too Low
+
+            if (userInput < favNumber)
+
+            {
+                Console.WriteLine("Too Low");
+
+
+            }
+
+            // If user input is greater than print Too High
+
+            else if (userInput > favNumber)
+
+            {
+                Console.WriteLine("Too High");
+
+            }
+
+            // If user input is neither print Nevermind
+
+            else 
+
+            {
+                Console.WriteLine("Nevermind");
+
+
+            }
+
+
+
+
+
         }
     }
 }
